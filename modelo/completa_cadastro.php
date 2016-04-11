a59c919b699910a816a6c8c496071dfa7b91dd0a
Felipe Vogel <feoli7@hotmail.com>
2016-04-11 15:10:20 -0300

    Added files via upload

--- modelo/completa_cadastro.php
+++ modelo/completa_cadastro.php
@@ -3,14 +3,95 @@
 $link = mysqli_connect("localhost", "root", "", "laps");
 if(!empty($_GET['recusa']))
 {
-	$user = $_GET['id_cliente'];
-	$sql = "UPDATE Cliente  set cadastro = 0 where id_cliente = ".$user;
+	$cliente = $_GET['id_cliente'];
+	$comentario = $_GET['comentario'];
+	$sql = "UPDATE Cliente  set cadastro = 0 where id_cliente = '".$cliente."'";
 	if (mysqli_query($link, $sql)) { //echo "inserido";
 	}
+	$sql = "UPDATE Conta  set comentario = '".$comentario."' where id_cliente = '".$cliente."'";
+	if (mysqli_query($link, $sql)) { //echo "inserido";
+	}
+    $from = 'lapsuvv@gmail.com';
 	$mail = $_GET['email'];
-	include("solicitacao_recusa.php");
-	$volta = $_SERVER['HTTP_REFERER'];
-	echo "<script>window.location='$volta';alert('$nome, Cliente Rejeitado com Sucesso! Uma mensagem foi enviada');</script>";
+
+require_once("../phpmailer/class.phpmailer.php");
+
+define('GUSER', $from);	// <-- Insira aqui o seu GMail
+define('GPWD', 'lapsuvv2016');		// <-- Insira aqui a senha do seu GMail
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsHTML(true);
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 1;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 465;  		// A porta 465 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Mensagem enviada!';
+		return true;
+	}
+}
+$msg = '
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>SOLICITAR PARTICIPAÇÃO</title>
+<style id="jsbin-css">
+#t1{
+  background: #FFFFFF;
+  border: 2px solid;
+  font-family: calibri;
+  font-size: 20px; 
+
+}
+</style>
+</head>
+<body>
+<table id="t1" width="800"> 
+<tr>
+<td>
+  <img src="http://lapsapp.github.io/images/assign1.jpg"><br>
+<table id="t2" width="800"> 
+<tr>
+<td width="500">
+  <p align="Justify"><b>Prezado(a) usuário! </b>
+  <p align="Justify">Infelizmente, sua solicitação de cadastro foi recusada.<br> 
+Você possui pendências em seu nome, tente futuramente.
+</td>
+<td width="300" align="center"><img src="http://lapsapp.github.io/images/laps.png" width="140" height="160">
+</td>
+</tr>
+</table>
+<br><br>'.$comentario.'<br><br>
+<p align="center">Atenciosamente,</p>
+<p align="center"><b>EQUIPE LAPS</b><br><br></p>
+  <img src="http://lapsapp.github.io/images/assign2.jpg">
+</td>
+</tr>
+</table>
+</body>
+</html>';
+
+if (smtpmailer($mail, $from, 'LAPS', 'LAPS - Cliente Recusado', $msg)) {
+    $volta = $_SERVER['HTTP_REFERER'];
+    echo "<script>window.location='$volta';alert('Cliente Rejeitado com Sucesso! Uma mensagem foi enviada');</script>";
+
+}
+if (!empty($error)) echo $error;
+
 
 }
 else
@@ -23,11 +104,15 @@ else
 	$para = $_POST['email'];
 	$nome = $_POST['nome'];
 	$cliente = $_POST['id_cliente'];
+	$comentario = $_POST['comentario'];
 
 	if (!$link) {
 		die('Não foi possível conectar: '.mysql_error());
 	}
 
+    $sql = "UPDATE Conta  set comentario = '".$comentario."' where id_cliente = '".$cliente."'";
+	if (mysqli_query($link, $sql)) { //echo "inserido";
+	}
 
 	$sql = "INSERT into cartao(bandeira, codigo, criacao,id_conta, nome_cliente, numero, validade)values('UVV',
 		".$codseg.",
@@ -122,7 +207,7 @@ else
 		Limite: '.$limite.'<br>
 		Validade: '.$validade.'<br>
 		Código de Segurança: '.$codseg.'<br>
-		<br>
+		<br><br>'.$comentario.'<br><br>
 		<p align="Justify">Com o LAPS você poderá acompanhar em tempo real suas compras, visualizar seu limite disponível, gerar boletos de sua fatura, realizar bloqueios e desbloqueios do seu cartão, tudo online.
 		<br><br>
 		<p align="center">Atenciosamente,</p>
@@ -134,7 +219,7 @@ else
 		</body>
 		</html>';
 
-		if (smtpmailer($mail, $from, 'LAPS', 'Cliente Aprovado', $msg)) {
+		if (smtpmailer($mail, $from, 'LAPS', 'LAPS - Cliente Aprovado', $msg)) {
 			$volta = $_SERVER['HTTP_REFERER'];
 			echo "<script>window.location='$volta';alert('$nome, Cliente Aprovado com Sucesso! Uma mensagem foi enviada');</script>";
 		} else {
