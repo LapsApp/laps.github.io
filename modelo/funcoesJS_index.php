0eb7df4068a035172af96edf7b397b65f830fbf3
Alais <alaiscs@users.noreply.github.com>
2016-06-07 18:50:02 -0300

    correcoes

---
+++ modelo/funcoesJS_index.php
@@ -0,0 +1,20 @@
+<link href="./js/jAlert/src/jAlert-v3.css" rel="stylesheet">
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+<link href="./css/bootstrap-theme.css" rel="stylesheet">
+<link href="./css/elegant-icons-style.css" rel="stylesheet" />
+<link href="./css/font-awesome.min.css" rel="stylesheet" />
+<link href="./css/style.css" rel="stylesheet">
+<link href="./css/style-responsive.css" rel="stylesheet" />
+
+<script src="./js/jquery.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/jquery.scrollTo.min.js"></script>
+<script src="./js/jquery.nicescroll.js" type="text/javascript"></script>
+<script src="./js/jquery.validate.min.js" type="text/javascript" ></script>
+<script src="./js/form-validation-script.js"></script>
+<script src="./js/scripts.js"></script>
+<script src="./js/jquery.singlePageNav.min.js"></script>
+<script src="./js/wow.min.js"></script>
+<script src="./js/custom.js"></script>
+<script src="./js/jAlert/src/jAlert-functions.js"></script>
+<script src="./js/jAlert/src/jAlert-v3.js"></script>
